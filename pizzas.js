08b1a2d7c6206c9ae25e3009503bf5fca3556170
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2020-12-29 16:42:54 -0300

    Parte 3
    Nesse commit foi feito adicionado as imagens, o nome das pizzas, o preço e a descrição de cada pizza

--- pizzas.js
+++ pizzas.js
@@ -1,8 +1,8 @@
 let pizzaJson = [
     {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Molho de tomate, mussarela, tomate em rodelas e orégano.'},
     {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa e orégano.'},
-    {id:3, name:'Cinco Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, parmesão, provolone, gorgonzola, catupiry e orégano.'},
-    {id:4, name:'Brocóli', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, brócolis, catupiry e orégano.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, parmesão, provolone, gorgonzola  e orégano.'},
+    {id:4, name:'Brocólis', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, brócolis, catupiry e orégano.'},
     {id:5, name:'Sensação', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Queijo mussarela, molho doce e chocolate preto/branco e morango.'},
     {id:6, name:'Catuperu', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, peito de peru defumado, catupiry e orégano.'},
     {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Queijo mussarela, molho doce e chocolate preto.'}
--- script.js
+++ script.js
@@ -4,7 +4,9 @@ pizzaJson.map((item,index)=>{
 
     let pizzaitem = document.querySelector('.models .pizza-item').cloneNode(true);
 
+    pizzaitem.querySelector('.pizza-item--img img').src = item.img;
     pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}` ;
     pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description;
     
 
