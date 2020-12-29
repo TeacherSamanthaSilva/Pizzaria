cb47c3c4cd9a30e8baf9bf51978ece06aaf08263
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2020-12-29 15:23:17 -0300

    descrição
    Neste commit foi adicionado a descrição dos sabores de pizza

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const c = (el) =>{
 
-    document.querySelector(el);
-}
 
 pizzaJson.map((item,index)=>{
 
     let pizzaitem = document.querySelector('.models .pizza-item').cloneNode(true);
+
+    pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    
 
     document.querySelector('.pizza-area').append(pizzaitem);
 })
