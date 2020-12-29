58485ab22cb0854b1de5583c952eab438715bb2d
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2020-12-29 17:03:09 -0300

    animação
    Nesse commit foi feito uma animação para que ao clicar na pizza apareça um painel com algumas informações sobre aquela pizza que o usuário clicou

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ pizzaJson.map((item,index)=>{
     pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}` ;
     pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaitem.querySelector('a').addEventListener('click',(e)=>{
+
+        e.preventDefault();
+
+        document.querySelector('.pizzaWindowArea').style.opacity = 0;
+        document.querySelector('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            document.querySelector('.pizzaWindowArea').style.opacity = 1;
+        },200)
+
+        
+
+    })
     
 
     document.querySelector('.pizza-area').append(pizzaitem);
