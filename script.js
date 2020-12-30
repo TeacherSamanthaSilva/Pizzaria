d8c5b5544e9b40852898fcec25fc26866fd5465c
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2020-12-29 23:13:28 -0300

    Parte 5
    Nesse commit foi criado um evento que ao clicar na pizza aparece um painel com algumas informações sobre a pizza clicada

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ pizzaJson.map((item,index)=>{
 
     let pizzaitem = document.querySelector('.models .pizza-item').cloneNode(true);
 
+
+    pizzaitem.setAttribute('data-key',index)
     pizzaitem.querySelector('.pizza-item--img img').src = item.img;
     pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}` ;
@@ -11,6 +13,11 @@ pizzaJson.map((item,index)=>{
     pizzaitem.querySelector('a').addEventListener('click',(e)=>{
 
         e.preventDefault();
+
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
+        document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
 
         document.querySelector('.pizzaWindowArea').style.opacity = 0;
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
